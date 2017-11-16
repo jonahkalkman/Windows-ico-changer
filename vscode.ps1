43c37d1c2281e0490f471fb538bdbece74034faa
Jonah Kalkman <jonahkalkman@gmail.com>
2017-11-16 22:41:03 +0100

    Paths changed

--- vscode.ps1
+++ vscode.ps1
@@ -1,6 +1,6 @@
 $x = Get-Random -Minimum 0 -Maximum 15
-$x1 = Resolve-Path .\Desktop\Shortcutechanger\Windows-ico-changer\vscode.dll
+$x1 = Resolve-Path .\Desktop\Windows-ico-changer\vscode.dll
 $Shell = New-Object -ComObject ("WScript.Shell")
-$Shortcut = $Shell.CreateShortcut("C:\Users\Jonah Kalkman\Desktop\Visual Studio Code.lnk")
+$Shortcut = $Shell.CreateShortcut(".\Desktop\Visual Studio Code.lnk")
 $Shortcut.IconLocation = ("$x1, $x")
 $Shortcut.Save()
